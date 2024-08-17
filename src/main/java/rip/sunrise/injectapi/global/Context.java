Sunderw3k/InjectAPI

commit 69a01ac3b621dd1525c979ea9bfa56ef8fad0e66

fix: rewrite global classes to java
A few classes are loaded in all ClassLoaders. They should be written in java, so they don't rely on the kotlin stdlib.

src/main/java/rip/sunrise/injectapi/global/Context.java
@@ -1,0 +1,40 @@
+package rip.sunrise.injectapi.global;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * Passed into hooks, then used for the return value.
+ * <p>
+ * Note: Written in java because it is loaded in ALL hooked ClassLoaders.
+ */
+public class Context {
+    /**
+     * Specifies the early return value returned right after the hook callback.
+     */
+    // TODO: Change to null because its java? A null jump should be cheaper than this
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    public Optional<Object> returnValue = Optional.empty();
+
+    /**
+     * Because this class is used as a bridge between hooks and hooked code, it is necessary to serialize the object into something both classloaders can understand.
+     * To not lose ALL type information, a Map isn't the worst idea.
+     */
+    @SuppressWarnings("unused")
+    public @NotNull Map<String, Object> serialize() {
+        return Map.of("returnValue", returnValue);
+    }
+
+    /**
+     * Only static to make it easier to invoke from MethodHandles and bytecode.
+     */
+    @SuppressWarnings("unused")
+    public static @NotNull Context deserialize(@NotNull Map<String, Object> data) {
+        Context ctx = new Context();
+        ctx.returnValue = Optional.of(data.get("returnValue"));
+
+        return ctx;
+    }
+}

src/main/java/rip/sunrise/injectapi/global/DataTransport.java
@@ -1,0 +1,5 @@
+package rip.sunrise.injectapi.global;
+
+public class DataTransport {
+    public static ClassLoader classLoader;
+}

src/main/java/rip/sunrise/injectapi/global/ProxyDynamicFactory.java
@@ -1,0 +1,30 @@
+package rip.sunrise.injectapi.global;
+
+import java.lang.invoke.*;
+
+public class ProxyDynamicFactory {
+    public static CallSite bootstrap(MethodHandles.Lookup caller, String name, MethodType type, int hookId) {
+        System.out.println("Got invokedynamic call from" + caller + " for hookId " + hookId);
+
+        try {
+            ClassLoader cl = ((ClassLoader) ClassLoader.getSystemClassLoader()
+                    .loadClass("rip.sunrise.injectapi.global.DataTransport")
+                    .getDeclaredField("classLoader")
+                    .get(null));
+
+            Class<?> managerClass = cl.loadClass("rip.sunrise.injectapi.managers.HookManager");
+            Class<?> hookClass = cl.loadClass("rip.sunrise.injectapi.hooks.Hook");
+
+            Object instance = managerClass.getDeclaredField("INSTANCE").get(null);
+
+            Object hook = managerClass.getDeclaredMethod("getHook", int.class).invoke(instance, hookId);
+
+            MethodHandle handle = (MethodHandle) hookClass.getDeclaredMethod("getHandle").invoke(hook);
+
+            // TODO: Kinda forced, it won't fail. Only happens because types are lost when calling unreflect.
+            return new ConstantCallSite(handle.asType(type));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

src/main/kotlin/rip/sunrise/injectapi/InjectApi.kt
@@ -1,8 +1,8 @@
 package rip.sunrise.injectapi
 
 import org.objectweb.asm.Type
-import rip.sunrise.injectapi.callsite.ProxyDynamicFactory
-import rip.sunrise.injectapi.hooks.inject.Context
+import rip.sunrise.injectapi.global.Context
+import rip.sunrise.injectapi.global.ProxyDynamicFactory
 import rip.sunrise.injectapi.managers.HookManager
 import java.lang.instrument.Instrumentation
 
@@ -14,9 +14,11 @@ private external fun nativeDefineClass(loader: ClassLoader, classBytes: ByteArra
  * IMPORTANT: Functioning of the hooks depends on where this class is loaded. Make sure this is the same ClassLoader that loads HookManager.
  */
 object InjectApi {
-    const val CONTEXT_CLASS = "rip/sunrise/injectapi/hooks/inject/Context"
-    const val BOOTSTRAP_CLASS = "rip/sunrise/injectapi/callsite/ProxyDynamicFactory"
-    const val DATA_TRANSPORT_CLASS = "rip/sunrise/injectapi/callsite/DataTransport"
+    // TODO: This should be some kind of AP for compile-time inlining.
+    // I know const is inlining it, but I'm not sure if it works on java. Its better for me to write it myself.
+    const val CONTEXT_CLASS = "rip/sunrise/injectapi/global/Context"
+    const val BOOTSTRAP_CLASS = "rip/sunrise/injectapi/global/ProxyDynamicFactory"
+    const val DATA_TRANSPORT_CLASS = "rip/sunrise/injectapi/global/DataTransport"
 
     init {
         // TODO: Windows/Mac support.
@@ -48,7 +50,6 @@ object InjectApi {
             nativeDefineClass(it, getClassBytes(Type.getInternalName(ProxyDynamicFactory::class.java)))
 
             nativeDefineClass(it, getClassBytes(Type.getInternalName(Context::class.java)))
-            nativeDefineClass(it, getClassBytes(Type.getInternalName(Context.Companion::class.java)))
         }
 
         // Retransform
@@ -63,8 +64,8 @@ object InjectApi {
         // Set up the injection CL.
         ClassLoader.getSystemClassLoader()
             .loadClass(DATA_TRANSPORT_CLASS.replace("/", "."))
-            .getDeclaredMethod("setClassLoader", ClassLoader::class.java)
-            .invoke(null, InjectApi::class.java.classLoader)
+            .getDeclaredField("classLoader")
+            .set(null, InjectApi::class.java.classLoader)
     }
 
     private fun getClassBytes(name: String): ByteArray {

src/main/kotlin/rip/sunrise/injectapi/callsite/DataTransport.kt
@@ -1,12 +1,0 @@
-package rip.sunrise.injectapi.callsite
-
-/**
- * Because [ProxyDynamicFactory.bootstrap] needs a [ClassLoader] to use when loading HookManager, we need to somehow specify it.
- *
- * This class is loaded in the System CL and is accessed so that we can properly load HookManager.
- */
-@Suppress("unused")
-object DataTransport {
-    @JvmStatic
-    lateinit var classLoader: ClassLoader
-}

src/main/kotlin/rip/sunrise/injectapi/callsite/ProxyDynamicFactory.kt
@@ -1,41 +1,0 @@
-package rip.sunrise.injectapi.callsite
-
-import rip.sunrise.injectapi.hooks.inject.Context
-import java.lang.invoke.*
-
-/**
- * Class containing a bootstrap method for all the hooks.
- *
- * NOTE: Loaded on ALL hooked ClassLoaders.
- */
-object ProxyDynamicFactory {
-    /**
-     * This method is called as a bootstrap for invokedynamic in hooked code. The returned [CallSite] points to the hooks [MethodHandle].
-     */
-    @JvmStatic
-    fun bootstrap(caller: MethodHandles.Lookup, name: String, type: MethodType, hookId: Int): CallSite {
-        try {
-            println("Got invokedynamic call from $caller for hookId $hookId")
-
-            val cl = ClassLoader.getSystemClassLoader()
-                .loadClass("rip.sunrise.injectapi.callsite.DataTransport")
-                .getDeclaredMethod("getClassLoader")
-                .invoke(null) as ClassLoader
-
-            val clazz = cl.loadClass("rip.sunrise.injectapi.managers.HookManager")
-            val hookClass = cl.loadClass("rip.sunrise.injectapi.hooks.Hook")
-
-            // Note: DO NOT CAST. This is most likely not the correct classloader. current != cl
-            val instance = clazz.getDeclaredField("INSTANCE").get(null)
-
-            val hook = clazz.getDeclaredMethod("getHook", Int::class.java).invoke(instance, hookId)
-            val handle = hookClass.getDeclaredMethod("getHandle").invoke(hook) as MethodHandle
-
-            // TODO: Kinda forced, it won't fail. Only happens because types are lost when calling unreflect.
-            return ConstantCallSite(handle.asType(type))
-        } catch (e: Exception) {
-            e.printStackTrace()
-            error(e)
-        }
-    }
-}

src/main/kotlin/rip/sunrise/injectapi/hooks/inject/Context.kt
@@ -1,39 +1,0 @@
-package rip.sunrise.injectapi.hooks.inject
-
-import java.util.*
-
-/**
- * Passed into hooks, then used for the return value.
- *
- * NOTE: Loaded on ALL hooked ClassLoaders.
- */
-class Context {
-    /**
-     * Specifies the early return value returned right after the hook callback.
-     */
-    var returnValue = Optional.empty<Any>()
-
-    /**
-     * Because this class is used as a bridge between hooks and hooked code, it is necessary to serialize the object into something both classloaders can understand.
-     * To not lose ALL type information, a Map<String, Any> isn't the worst idea.
-     */
-    @Suppress("unused")
-    fun serialize(): Map<String, Any> {
-        return mapOf("returnValue" to returnValue)
-    }
-
-    companion object {
-        /**
-         * Only static to make it easier to invoke from MethodHandles and bytecode.
-         *
-         * Note: The Companion class has to be loaded in all classloaders too, because the static version in Context invokes this virtual one.
-         */
-        @JvmStatic
-        @Suppress("unused")
-        fun deserialize(data: Map<String, Any>): Context {
-            return Context().also {
-                it.returnValue = data["returnValue"] as Optional<Any>
-            }
-        }
-    }
-}

src/main/kotlin/rip/sunrise/injectapi/transformers/InjectTransformer.kt
@@ -97,17 +97,17 @@ class InjectTransformer {
      *
      * @see InjectHook.arguments
      * @see HookManager.getHookId
-     * @see rip.sunrise.injectapi.callsite.ProxyDynamicFactory.bootstrap
+     * @see rip.sunrise.injectapi.global.ProxyDynamicFactory.bootstrap
      */
     private fun generateHookCode(hook: InjectHook, method: MethodNode): InsnList {
         return InsnList().apply {
             // Initialize Context
             add(TypeInsnNode(Opcodes.NEW, InjectApi.CONTEXT_CLASS))
             add(InsnNode(Opcodes.DUP))
-            add(MethodInsnNode(Opcodes.INVOKESPECIAL, InjectApi.CONTEXT_CLASS, "<init>", "()V"))
+            add(MethodInsnNode(Opcodes.INVOKESPECIAL, InjectApi.CONTEXT_CLASS, "<init>", "()V", false))
 
             // Serialize
-            add(MethodInsnNode(Opcodes.INVOKEVIRTUAL, InjectApi.CONTEXT_CLASS, "serialize", "()Ljava/util/Map;"))
+            add(MethodInsnNode(Opcodes.INVOKEVIRTUAL, InjectApi.CONTEXT_CLASS, "serialize", "()Ljava/util/Map;", false))
 
             // Load args
             hook.arguments.forEach {
@@ -137,10 +137,10 @@ class InjectTransformer {
             ))
 
             // Deserialize
-            add(MethodInsnNode(Opcodes.INVOKESTATIC, InjectApi.CONTEXT_CLASS, "deserialize", "(Ljava/util/Map;)L${InjectApi.CONTEXT_CLASS};"))
+            add(MethodInsnNode(Opcodes.INVOKESTATIC, InjectApi.CONTEXT_CLASS, "deserialize", "(Ljava/util/Map;)L${InjectApi.CONTEXT_CLASS};", false))
 
             // Get optional
-            add(MethodInsnNode(Opcodes.INVOKEVIRTUAL, InjectApi.CONTEXT_CLASS, "getReturnValue", "()Ljava/util/Optional;"))
+            add(FieldInsnNode(Opcodes.GETFIELD, InjectApi.CONTEXT_CLASS, "returnValue", "Ljava/util/Optional;"))
             add(InsnNode(Opcodes.DUP))
 
             // Get optional value and check if its present

commit 826232f712c2dda53b2795208bfb1e3062c4e3fb

fix: correct Optional always being present caused by 69a01ac3

src/main/java/rip/sunrise/injectapi/global/Context.java
@@ -14,7 +14,6 @@ public class Context {
     /**
      * Specifies the early return value returned right after the hook callback.
      */
-    // TODO: Change to null because its java? A null jump should be cheaper than this
     @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
     public Optional<Object> returnValue = Optional.empty();
 
@@ -30,10 +29,10 @@ public class Context {
     /**
      * Only static to make it easier to invoke from MethodHandles and bytecode.
      */
-    @SuppressWarnings("unused")
+    @SuppressWarnings({"unused", "unchecked"})
     public static @NotNull Context deserialize(@NotNull Map<String, Object> data) {
         Context ctx = new Context();
-        ctx.returnValue = Optional.of(data.get("returnValue"));
+        ctx.returnValue = (Optional<Object>) data.get("returnValue");
 
         return ctx;
     }