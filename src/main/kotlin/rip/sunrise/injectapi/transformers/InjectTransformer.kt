Sunderw3k/InjectAPI

commit fac04dddc24cc5e640292539b4c9a4cdf7e4b0f6

feat: remove all dependency on LocalVariableTable
Also, to make tests stricter, compile them with `-g:none`.

src/main/kotlin/rip/sunrise/injectapi/transformers/InjectTransformer.kt
@@ -24,14 +24,10 @@ class InjectTransformer {
                 .filter { it.method.name == method.name && it.method.desc == method.desc }
                 .sortedBy { it.injectionMode.typePriority }
                 .forEach { hook ->
-                    if (!hook.validateArguments(method.localVariables as List<LocalVariableNode>)) {
-                        // TODO: Error doesnt work. This is wild
-                        println("Hook '$hook' attempts to capture invalid arguments")
-                    }
-
+                    val hookCode = generateHookCode(hook, method, node)
                     when (hook.injectionMode) {
                         is HeadInjection -> {
-                            method.instructions.insert(generateHookCode(hook, method))
+                            method.instructions.insert(hookCode)
                         }
 
                         is ReturnInjection -> {
@@ -49,7 +45,7 @@ class InjectTransformer {
                                         Opcodes.DRETURN
                                     )
                                 }.forEach {
-                                    method.instructions.insertBefore(it, generateHookCode(hook, method))
+                                    method.instructions.insertBefore(it, hookCode)
                                 }
                         }
 
@@ -63,7 +59,7 @@ class InjectTransformer {
                                     val index = method.instructions.indexOf(it)
                                     val offset = method.instructions.get(index + hook.injectionMode.offset)
 
-                                    method.instructions.insert(offset, generateHookCode(hook, method))
+                                    method.instructions.insert(offset, hookCode)
                                 }
                         }
                     }
@@ -92,7 +88,7 @@ class InjectTransformer {
      * @see HookManager.getHookId
      * @see rip.sunrise.injectapi.global.ProxyDynamicFactory.bootstrap
      */
-    private fun generateHookCode(hook: InjectHook, method: MethodNode): InsnList {
+    private fun generateHookCode(hook: InjectHook, method: MethodNode, clazz: ClassNode): InsnList {
         val contextClass = "@CONTEXT@"
         val hookId = HookManager.getHookId(hook)
 
@@ -144,8 +140,8 @@ class InjectTransformer {
                 ).toMethodDescriptorString(),
                 false
             )
-            val capturedDescriptor =
-                getCapturedDescriptor(method.localVariables as List<LocalVariableNode>, hook.arguments)
+
+            val capturedDescriptor = getCapturedDescriptor(hook.arguments, method, clazz.name)
             add(
                 InvokeDynamicInsnNode(
                     "INJECT",

Justfile
@@ -12,7 +12,7 @@ build test_name:
 
 	cp "{{LIBRARY_PATH}}" InjectAPI-all.jar
 
-	javac -d . ../App.java
+	javac -g:none -d . ../App.java
 	jar -cf App.jar App.class
 
 	kotlinc -jvm-target 21 -include-runtime -cp "InjectAPI-all.jar:App.jar" -d Agent.jar ../Agent.kt

src/main/kotlin/rip/sunrise/injectapi/transformers/RedirectTransformer.kt
@@ -27,7 +27,7 @@ class RedirectTransformer {
                         .filter { it.name == hook.targetField.name && it.desc == hook.targetField.type }
                         .filter { it.opcode in hook.type.allowedOpcodes }
                         .forEach {
-                            val hookCode = generateHookCode(hook, it, method)
+                            val hookCode = generateHookCode(hook, it, method, node)
 
                             when (it.opcode) {
                                 in listOf(Opcodes.GETFIELD, Opcodes.GETSTATIC) -> {
@@ -45,7 +45,7 @@ class RedirectTransformer {
         }
     }
 
-    private fun generateHookCode(hook: FieldRedirectHook, field: FieldInsnNode, method: MethodNode): InsnList {
+    private fun generateHookCode(hook: FieldRedirectHook, field: FieldInsnNode, method: MethodNode, clazz: ClassNode): InsnList {
         val hookId = HookManager.getHookId(hook)
 
         return InsnList().apply {
@@ -90,7 +90,7 @@ class RedirectTransformer {
             )
 
             val fieldType = field.desc
-            val capturedDescriptor = getCapturedDescriptor(method.localVariables, hook.arguments)
+            val capturedDescriptor = getCapturedDescriptor(hook.arguments, method, clazz.name)
             add(InvokeDynamicInsnNode(
                 "REDIRECT_FIELD",
                 "($fieldType$capturedDescriptor)$fieldType",

src/main/kotlin/rip/sunrise/injectapi/utils/TypeUtils.kt
@@ -2,11 +2,7 @@ package rip.sunrise.injectapi.utils
 
 import org.objectweb.asm.Opcodes
 import org.objectweb.asm.Type
-import org.objectweb.asm.tree.InsnList
-import org.objectweb.asm.tree.InsnNode
-import org.objectweb.asm.tree.LocalVariableNode
-import org.objectweb.asm.tree.MethodInsnNode
-import org.objectweb.asm.tree.TypeInsnNode
+import org.objectweb.asm.tree.*
 import rip.sunrise.injectapi.hooks.CapturedArgument
 
 /**
@@ -44,8 +40,23 @@ fun getCheckCastReturnBytecode(type: Type): InsnList {
 }
 
 /**
- * Returns the descriptor of the parameters from known method [variables] and requested [capturedArguments].
+ * Returns the descriptor of the parameters from requested [capturedArguments].
  */
-fun getCapturedDescriptor(variables: List<LocalVariableNode>, capturedArguments: List<CapturedArgument>): String {
-    return capturedArguments.joinToString("") { variables.first { variable -> variable.index == it.index }.desc }
+fun getCapturedDescriptor(
+    capturedArguments: List<CapturedArgument>,
+    method: MethodNode,
+    classType: String
+): String {
+    // TODO: Some sanity checking if the args are valid
+
+    val arguments = Type.getArgumentTypes(method.desc)
+    val isVirtual = method.access and Opcodes.ACC_STATIC == 0
+
+    return capturedArguments.joinToString("") {
+        if (isVirtual && it.index == 0) {
+            "L$classType;" // this
+        } else if (isVirtual) {
+            arguments[it.index - 1].descriptor // virtual 1 is arguments[0]
+        } else arguments[it.index].descriptor // static 1 is arguments[1]
+    }
 }

commit 46ea072ff18f75f440b8e095a725b2f338266d30

fix: revert part of fac04dd which broke fbe800e

src/main/kotlin/rip/sunrise/injectapi/transformers/InjectTransformer.kt
@@ -25,10 +25,10 @@ class InjectTransformer {
                 .filter { it.method.name == method.name && it.method.desc == method.desc }
                 .sortedBy { it.injectionMode.typePriority }
                 .forEach { hook ->
-                    val hookCode = generateHookCode(hook, method, node)
+                    // NOTE: Keep the generateHookCode inlined, ASM doesn't like repeating labels.
                     when (hook.injectionMode) {
                         is HeadInjection -> {
-                            method.instructions.insert(hookCode)
+                            method.instructions.insert(generateHookCode(hook, method, node))
                         }
 
                         is ReturnInjection -> {
@@ -46,7 +46,7 @@ class InjectTransformer {
                                         Opcodes.DRETURN
                                     )
                                 }.forEach {
-                                    method.instructions.insertBefore(it, hookCode)
+                                    method.instructions.insertBefore(it, generateHookCode(hook, method, node))
                                 }
                         }
 
@@ -60,7 +60,7 @@ class InjectTransformer {
                                     val index = method.instructions.indexOf(it)
                                     val offset = method.instructions.get(index + hook.injectionMode.offset)
 
-                                    method.instructions.insert(offset, hookCode)
+                                    method.instructions.insert(offset, generateHookCode(hook, method, node))
                                 }
                         }
                     }