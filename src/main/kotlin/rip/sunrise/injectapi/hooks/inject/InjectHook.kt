853f7a34b88c2095c0a98f9309c0578f96c32f82
Sunderw <73128678+Sunderw3k@users.noreply.github.com>
2025-11-23 15:29:52 +0100

    chore: remove unsued validateArguments method

--- src/main/kotlin/rip/sunrise/injectapi/hooks/inject/InjectHook.kt
+++ src/main/kotlin/rip/sunrise/injectapi/hooks/inject/InjectHook.kt
@@ -29,19 +29,6 @@ class InjectHook(
         arguments: List<CapturedArgument>,
         hook: Function<Unit>
     ) : this(injectionMode, clazz, method, arguments, hook.toMethodHandle())
-
-    /**
-     * Validates whether [arguments] are valid given the known local [variables].
-     */
-    fun validateArguments(variables: List<LocalVariableNode>): Boolean {
-        // TODO: Breaks when LocalVariableTable is not there.
-
-        // TODO: Using reflection loses the types. Check out FunctionExtension
-        // The best I can do right now is check bounds and opcodes. But opcodes are annoying.
-
-        val variableIndices = variables.map { it.index }
-        return arguments.all { it.index in variableIndices }
-    }
 }
 
 /**
